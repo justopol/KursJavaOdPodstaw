7bd00bfb83d5372748b33b8ef96be93251b1e4f2
Justyna Opolska <justopol@wp.pl>
2022-03-28 16:44:04 +0200

    testowy commit

--- com/klasy_abstrakcyjne_interfejsy_inne/PrzykladowaKlasaAbstrakcyjna.java
+++ com/klasy_abstrakcyjne_interfejsy_inne/PrzykladowaKlasaAbstrakcyjna.java
@@ -5,7 +5,7 @@ public abstract class PrzykladowaKlasaAbstrakcyjna {
     public void jakasMetoda(){
         System.out.println("cos na ekran");
     }
-    public abstract void  metodaAbstrakcyjna();// towrzenie przez slowo abstract i nie moze miec ciala, cialo implementujemy w klasie ktora dziedziczy po abstrakcyjnejn
+    public abstract void  metodaAbstrakcyjna();//towrzenie przez slowo abstract i nie moze miec ciala, cialo implementujemy w klasie ktora dziedziczy po abstrakcyjnejn
     public abstract int metodaZParametrem(String z);
 
 }
