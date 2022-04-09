34b9095454b737e1221bfdfd3bf09c5b48f93061
Justyna Opolska <justopol@wp.pl>
2022-04-09 12:35:43 +0200

    stworzenie planszy

--- com/zadania_rekrutacja/kolko_krzyzyk/Gra.java
+++ com/zadania_rekrutacja/kolko_krzyzyk/Gra.java
@@ -1,16 +1,30 @@
 package com.zadania_rekrutacja.kolko_krzyzyk;
 
+import java.util.Random;
+
 public class Gra {
     private String[][] plansza;//deklaracja + przypisanie = inicjalizacja
     private final static int wymiaryPlanszy = 3;
+    private String gracz;
+
     public Gra(){
       plansza = new String[wymiaryPlanszy][wymiaryPlanszy];
       for( int i =0; i < wymiaryPlanszy; i++){
           for( int j =0; j < wymiaryPlanszy; j++){
               plansza [i][j]= " ";
           }
-
       }
+
+      Random r = new Random();
+      int liczba = r.nextInt(11);
+      if(liczba%2  == 0){
+          System.out.println("Zaczyna gracz x.");
+          gracz = "x";
+      }else{
+          System.out.println("Zaczyna gracz o.");
+          gracz = "o";
+      }
+
 
     }
 
