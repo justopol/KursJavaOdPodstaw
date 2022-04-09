b53dd352b1e0f7d2c6d9b4dfddf862a9bc3b16d2
Justyna Opolska <justopol@wp.pl>
2022-04-09 13:03:02 +0200

    stworzenie mozliwosci wypisywania znakow przez gracza

--- com/zadania_rekrutacja/kolko_krzyzyk/Gra.java
+++ com/zadania_rekrutacja/kolko_krzyzyk/Gra.java
@@ -5,27 +5,46 @@ import java.util.Random;
 public class Gra {
     private String[][] plansza;//deklaracja + przypisanie = inicjalizacja
     private final static int wymiaryPlanszy = 3;
-    private String gracz;
+    private String znak;
 
     public Gra(){
       plansza = new String[wymiaryPlanszy][wymiaryPlanszy];
-      for( int i =0; i < wymiaryPlanszy; i++){
-          for( int j =0; j < wymiaryPlanszy; j++){
-              plansza [i][j]= " ";
-          }
-      }
+        stworzPustaPlansze();
+        losujGracza();
+    }
+    public void wybierzPole(int pion, int poziom){
+        plansza[pion][poziom] = znak + " ";
+    }
+    public void wyswietlPlansze(){
+        for( int i =0; i < wymiaryPlanszy; i++){
+            for( int j =0; j < wymiaryPlanszy; j++){
+                System.out.print(plansza[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
-      Random r = new Random();
-      int liczba = r.nextInt(10);
-      if(liczba%2  == 0){
-          System.out.println("Zaczyna gracz x.");
-          gracz = "x";
-      }else{
-          System.out.println("Zaczyna gracz o.");
-          gracz = "o";
-      }
+    private void stworzPustaPlansze() {
+        for( int i =0; i < wymiaryPlanszy; i++){
+            for( int j =0; j < wymiaryPlanszy; j++){
+                plansza [i][j]= "- ";
+            }
+        }
+    }
 
+    private void losujGracza() {
+        Random r = new Random();
+        int liczba = r.nextInt(10);
+        if(liczba%2  == 0){
+            System.out.println("Zaczyna gracz x.");
+            znak = "x";
+        }else{
+            System.out.println("Zaczyna gracz o.");
+            znak = "o";
+        }
 
     }
+
+
 
 }
--- com/zadania_rekrutacja/kolko_krzyzyk/MainKoloKrzyzyk.java
+++ com/zadania_rekrutacja/kolko_krzyzyk/MainKoloKrzyzyk.java
@@ -3,6 +3,8 @@ package com.zadania_rekrutacja.kolko_krzyzyk;
 public class MainKoloKrzyzyk {
     public static void main(String[]args){
         Gra gra = new Gra();
+        gra.wybierzPole(2,1);
+        gra.wyswietlPlansze();
 
 
     }
